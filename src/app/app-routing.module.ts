309ddb7fa82e9940c65ca9f1cf7f2beda3dacd34
Mustakim-Khan <mkhan@myges.fr>
2021-12-05 12:43:46 +0100

    dimple dashboard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {DashboardComponent} from "./views/dashboard/dashboard.component";
+import {PresentationComponent} from "./views/presentation/presentation.component";
+import {EducationComponent} from "./views/education/education.component";
+import {ProjectsComponent} from "./views/projects/projects.component";
+import {ContactComponent} from "./views/contact/contact.component";
 
 const routes: Routes = [
-  { path: '', pathMatch: 'full', component: DashboardComponent },
+  { path: '', pathMatch: 'full', component: DashboardComponent, data: { animation: 'isLeft' } },
+  { path: 'presentation', component: PresentationComponent, data: { animation: 'isLeft' } },
+  { path: 'education', component: EducationComponent, data: { animation: 'isLeft' } },
+  { path: 'projects', component: ProjectsComponent, data: { animation: 'isRight' } },
+  { path: 'contact', component: ContactComponent, data: { animation: 'isRight' } },
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,19 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {AngularMaterialModule} from "./modules/angular-material.module";
 import { DashboardComponent } from './views/dashboard/dashboard.component';
+import { PresentationComponent } from './views/presentation/presentation.component';
+import { EducationComponent } from './views/education/education.component';
+import { ProjectsComponent } from './views/projects/projects.component';
+import { ContactComponent } from './views/contact/contact.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DashboardComponent
+    DashboardComponent,
+    PresentationComponent,
+    EducationComponent,
+    ProjectsComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
