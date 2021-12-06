0005aa3db04b9e9d178a7ea97493b563d9ffda5f
Mustakim-Khan <mkhan@myges.fr>
2021-12-06 17:26:57 +0100

    dashboard images well cropped

--- src/app/views/presentation/presentation.component.ts
+++ src/app/views/presentation/presentation.component.ts
@@ -6,10 +6,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./presentation.component.scss']
 })
 export class PresentationComponent implements OnInit {
+  private birthdate = new Date("1999-11-28");
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  get age() {
+    let timeDiff = Math.abs(Date.now() - this.birthdate.getTime());
+    let age = Math.floor((timeDiff / (1000 * 3600 * 24))/365.25);
+    return age;
+  }
+
+  downloadCV() {
+    console.log('ici')
+  }
 }
