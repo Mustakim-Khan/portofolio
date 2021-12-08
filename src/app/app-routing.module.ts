2c9fc9c3aa45a1872354953d6d5d83b7409ab73a
Mustakim-Khan <mkhan@myges.fr>
2021-12-08 17:56:37 +0100

    deleter router-outlet

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import {MainPageComponent} from "./views/main-page/main-page.component";
 
 const routes: Routes = [
-  { path: '', component: MainPageComponent },
+  /*{ path: '', component: MainPageComponent },*/
 ];
 
 @NgModule({
